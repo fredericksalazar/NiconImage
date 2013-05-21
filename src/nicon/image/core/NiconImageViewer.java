9271bfee29bf4b8b7b471df391e6a0355da02e00
frederick <frederick@frederick-Aspire-5536>
2013-05-21 14:44:49 -0500

    Se implementa el NiconHome desde donde el usuario podrá acceder a nusestro contenido online, ademas se implementan nuevos objetos para NiconHome

--- src/nicon/image/core/NiconImageViewer.java
+++ src/nicon/image/core/NiconImageViewer.java
@@ -6,7 +6,9 @@ package nicon.image.core;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import nicon.image.core.conf.NiconImageConf;
 
@@ -21,12 +23,57 @@ public class NiconImageViewer extends JPanel {
     private NiconImageConf niConfig;
     private static Image imageTemp;
     
+    private JButton NiconFace;
+    private JButton NiconWeb;
+    
+    private String urlHome="/nicon/image/gui/icons/background.png";
+
     public NiconImageViewer(){
         niConfig=NiconImageConf.getInstance();
         setSize(1000,505);
         setBackground(niConfig.getPanelColor());
-        icon=new ImageIcon(getClass().getResource("/nicon/image/gui/icons/background.png"));
-        image=icon.getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_AREA_AVERAGING);
+        loadNiconHome();
+    }
+    
+    /**
+     * Este metodo es el encargado de cargar el panel de bienvenida dentro del
+     * visor de imagenes, se usa cuando inicia la app o cuando se elimina una
+     * imágen del sistema.
+     */
+    protected  void loadNiconHome(){
+        icon=new ImageIcon(getClass().getResource("/nicon/image/gui/icons/NiconHome.png"));
+        image=icon.getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_FAST);
+        
+        NiconFace=new JButton();
+        NiconFace.setToolTipText("Siguenos en Facebook.com");
+        NiconFace.setBounds(700, 470, 30, 30);
+        NiconFace.setIcon(new ImageIcon(getClass().getResource(niConfig.getIconsPath()+"NiconFace.png")));
+        NiconFace.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NiconImageCore.openNiconURL(0);
+            }
+        });
+        
+        NiconWeb=new JButton();
+        NiconWeb.setToolTipText("Abrir nuestra página web");
+        NiconWeb.setBounds(750, 470, 30, 30);
+        NiconWeb.setIcon(new ImageIcon(getClass().getResource(niConfig.getIconsPath()+"NiconWeb.png")));
+        NiconWeb.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NiconImageCore.openNiconURL(1);
+            }
+        });
+        
+        setLayout(null);
+        add(NiconFace);
+        add(NiconWeb);
+    }
+    
+    public void cleanNiconViewer(){
+        this.remove(NiconFace);
+        this.remove(NiconWeb);        
     }
 
     public void setImage(ImageIcon image) {
@@ -35,6 +82,11 @@ public class NiconImageViewer extends JPanel {
         this.repaint();
     }
     
+    /**
+     * Este metodo retorna el valor de ancho de la imagen que esta siendo 
+     * vista en ese momento
+     * @return 
+     */
     public static int getWidthImage(){
         return imageTemp.getWidth(null);
     }
